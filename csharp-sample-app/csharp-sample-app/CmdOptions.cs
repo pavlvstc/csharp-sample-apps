21069947976227fdaa325aec8428a637a6d38721
Umang Mukesh Mehta <umang.mehta@affectiva.com>
2016-08-03 13:48:19 -0400

    Adding the sample apps

---
+++ csharp-sample-app/csharp-sample-app/CmdOptions.cs
@@ -0,0 +1,38 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_sample_app
+{
+    class CmdOptions
+    {
+        [Option('i', "input", Required = true, HelpText = "Input stream to be processed.")]
+        public string Input { get; set; }
+
+        [Option('d', "data", Required = true, HelpText = "Data DIrectory.")]
+        public string DataFolder { get; set; }
+
+        [Option('l', "license", Required = true, HelpText = "license file path.")]
+        public string LicenseFile { get; set; }
+
+        [Option('f', "numFaces", Required = false, DefaultValue = 1, HelpText = "Set Max Number of faces.")]
+        public int numFaces { get; set; }
+
+        [Option('m', "faceMode", Required = false, DefaultValue = 0, HelpText = "Set face detector mode.")]
+        public int faceMode { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
